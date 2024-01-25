d845ca35ceae6a377a90b994494797b30341caaa
pranav manikandan <112387943+pranavm181@users.noreply.github.com>
2024-01-25 12:28:27 +0530

    Update workers_data.dart

--- lib/workers_data.dart
+++ lib/workers_data.dart
@@ -3,9 +3,46 @@ final workersdata = [
     'id': '0',
     'title': 'Carpenter\'s',
     'discription':
-        'jnakj aj ddjk f kj fjkfn j fnk b jjjhjhhj jlfkjgsklgkslgjklgj nkkb kjjssddfdf hh gdshgb jfgh  gh ahbbfkbfnbf',
+        'jnakj aj ddjk f kj fjkfn j fnk b  gj nkkb kjjssddfdf hh gdshgb jfgh  gh ahbbfkbfnbf',
     'name': 'pranav manikandan opf ',
     'place': 'thali',
     'userImage': '',
   },
+    {
+    'id': '1',
+    'title': 'Electrician\'s',
+    'discription':
+        'jnakj aj ddjk f kj fjkfn j fnk b  gj nkkb kjjssddfdf hh gdshgb jfgh  gh ahbbfkbfnbf',
+    'name': ' Akash u  ',
+    'place': 'nellyvaay',
+    'userImage': '',
+  },
+  {
+    'id': '2',
+    'title': 'Plumber\'s',
+    'discription':
+        'jnakj aj ddjk f kj fjkfn j fnk b  gj nkkb kjjssddfdf hh gdshgb jfgh  gh ahbbfkbfnbf',
+    'name': 'Anoop k a   ',
+    'place': 'parlikaad',
+    'userImage': '',
+  },
+  {
+    'id': '3',
+    'title': 'Cleaner\'s',
+    'discription':
+        'jnakj aj ddjk f kj fjkfn j fnk b  gj nkkb kjjssddfdf hh gdshgb jfgh  gh ahbbfkbfnbf',
+    'name': 'Arjun aravindan   ',
+    'place': 'nenmara',
+    'userImage': '',
+  },
+  {
+    'id': '4',
+    'title': 'Carpenter\'s',
+    'discription':
+        'jnakj aj ddjk f kj fjkfn j fnk b  gj nkkb kjjssddfdf hh gdshgb jfgh  gh ahbbfkbfnbf',
+    'name': 'Anathakrishana   ',
+    'place': 'Thrissur',
+    'userImage': '',
+  },
+
 ];
